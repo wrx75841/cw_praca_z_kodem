ff2d0babeb94bd45457b20278939fa94394086c2
“wrx75841” <wrx75841@student.wsb.wroclaw.pl>
2023-05-14 13:45:15 +0200

    Tested Flask

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-  @app.route('/')
+@app.route('/')
 def index():
   return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
- app.run(debug=True)
+  app.run(debug=True)
